f5b2e19304641a1ae744a16aeed8be34ebf0cbf2
Fgunja <fran.gunja@gmail.com>
2024-12-05 12:40:36 +0100

    Drugi

---
+++ Projekt/quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="q-pa-md">
+    <q-ajax-bar
+      ref="bar"
+      position="bottom"
+      color="accent"
+      size="10px"
+      skip-hijack
+    />
+
+    <q-btn color="primary" label="Trigger" @click="trigger" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const bar = ref(null)
+
+    function trigger () {
+      const barRef = bar.value
+      barRef.start()
+
+      setTimeout(() => {
+        const barRef = bar.value
+        if (barRef) {
+          barRef.stop()
+        }
+      }, Math.random() * 3000 + 1000)
+    }
+
+    return {
+      bar,
+      trigger
+    }
+  }
+}
+</script>
